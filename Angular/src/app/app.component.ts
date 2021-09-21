3cdaed4098b02b8e770fd5eeb37fb99bd97e05f1
Dweep <dweep.programmer@gmail.com>
2021-09-22 00:56:43 +0530

    Angular: Module 4, 5, 6, 7 & 8

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
   template: `
     <div>
       <h1>{{ pageTitle }}</h1>
-      <div>My First Component</div>
+      <pm-products></pm-products>
     </div>
   `,
+  providers: [ProductService],
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
---
+++ Angular/src/app/products/product-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
+
+  // private _productService;
+  // constructor(productService: ProductService) {
+  //   this._productService = productService.getProducts();
+  // }
+
+  // The above code is same as this, but below code is shorter form of above code.
+  constructor(private _productService: ProductService) {}
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.products = this._productService.getProducts();
+    this.filteredProducts = this.products;
+  }
+}
